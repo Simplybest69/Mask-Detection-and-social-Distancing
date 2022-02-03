2640a2bd9b8ed9f030a1a8f466ca4ccabf604902
SaiGanesh <60283852+Simplybest69@users.noreply.github.com>
2022-02-03 09:52:35 +0530

    Delete Streamlit directory

--- Streamlit/setup.sh
+++
@@ -1,9 +0,0 @@
-/home/appuser/venv/bin/python -m pip install --upgrade pip
-mkdir -p ~/.streamlit/                                               
-echo "\                       
-[server]\n\                       
-port = $PORT\n\                       
-enableCORS = false\n\                       
-headless = true\n\                       
-\n\                       
-" > ~/.streamlit/config.toml
